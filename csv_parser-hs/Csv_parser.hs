b4e3918695af64290dc5258bea072cd7dec62672
meelis utt <meelis.utt@gmail.com>
2020-09-24 13:43:03 +0300

    Parser now takes file name as parameter and prints out the csv file.

--- csv_parser-hs/Csv_parser.hs
+++ csv_parser-hs/Csv_parser.hs
@@ -1,14 +1,26 @@
 module Csv_parser where
 
-import Data.List.Split
--- splitOn
+import System.Environment -- getArgs
+import System.IO  
+import Control.Monad
+import Data.List.Split -- splitOn
 
 -- Goal:
 -- Make a matrix, where each matrix row corresponds to row in csv file.
 
--- test data -- csv = "date,exp,type,desc\n2020-01-01,20,test2,test\n2020-01-01,20,test,test\n2020-01-06,20,test,test\n2020-02-01,20,test,test\n"
+-- test data -- 
+-- csv = "date,exp,type,desc\n2020-01-01,20,test2,test\n2020-01-01,20,test,test\n2020-01-06,20,test,test\n2020-02-01,20,test,test\n"
 
+-- Description of function:
+-- split contents on lines
+-- remove first line (header)
+-- remove empty lines
+-- split each line on commas (",")
 csvLines :: String -> [[String]]
-csvLines csv = map (splitOn ",") $ filter (\x -> (/=) "" $ take 1 x) $ tail $ splitOn "\n" csv
+csvLines csv = map (splitOn ",") $ filter (\x -> (/=) "" $ take 1 x) $ tail $ lines csv
 
+main = do
+ [file] <- getArgs
+ contents <- readFile file --"data.csv"
+ print . csvLines $ contents
 
