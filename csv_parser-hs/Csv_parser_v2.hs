30c51dd4ca8d18c5f4ad96dc89b70913cab9a8f2
meelis utt <meelis.utt@gmail.com>
2020-10-10 17:11:58 +0300

    Simple csv parser implemented using Data.Text instead of String.

---
+++ csv_parser-hs/Csv_parser_v2.hs
@@ -0,0 +1,30 @@
+module Csv_parser_v2 where
+
+-- version, that uses Data.Text instead of String
+-- import System.IO
+import System.Environment -- getArgs
+
+import qualified Data.Text as T --hoogle Data.Text 2020-10-10: This module is intended to be imported qualified, to avoid name clashes with Prelude functions
+
+csv = T.pack "date,exp,type,desc\n2020-01-01,20,test2,test\n2020-01-01,20,test,test\n2020-01-06,20,test,test\n2020-02-01,20,test,test\n\n"
+
+-- Goal: read in .csv file and store the file in a matrix form, where each element is a Text type object, that can be further parsed as necessary.
+
+-- Break Text from newline characters ('\n')
+-- remove header
+-- remove empty lines
+
+csvLines :: T.Text -> [[T.Text]]
+csvLines csv =  map  (T.splitOn $ T.pack ",") $ filter (\x -> (/=) (T.pack "") $ T.take 1 x) $ tail $ T.lines csv
+
+parser_compiled :: IO [[T.Text]]
+parser_compiled = do
+ [file] <- getArgs
+ contents <- readFile file 
+ return . csvLines $ T.pack contents
+
+parser :: FilePath -> IO [[T.Text]]
+parser path = do
+ contents <- readFile path 
+ return . csvLines $ T.pack contents
+
