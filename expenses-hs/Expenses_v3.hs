f7ced332b2e906d380e4709ac6e157329ed05f5d
meelis utt <meelis.utt@gmail.com>
2020-10-26 17:01:38 +0200

    Refactored some functions; implemented reporting. -- adding expense to state and writing new expense is yet to implemented.

--- expenses-hs/Expenses_v3.hs
+++ expenses-hs/Expenses_v3.hs
@@ -5,7 +5,9 @@ module Expenses_v3 where
 -- refactor maybe
 
 import qualified Data.Text as T
+import Text.Read -- readMaybe
 import Control.Monad -- forM_
+import Control.Monad.State -- State
 import Csv_parser_v2 -- self written simple parser
 
 -- Define new data type Expense
@@ -16,105 +18,211 @@ data Expense = E {
  , description :: T.Text
 } deriving (Show,Eq)
 
+-- Define ordering by date only.
+instance Ord Expense where
+ (<=) E {date = d1} E{date = d2} = (<=) d1 d2
+ -- compare E (date _ _ _) (E date2 _ _ _) = if date == date2 then EQ
+ -- compare E {date = d1} E{date = d2} = if d1 == d2 then EQ
+ --  else if d1 <= d2 then LT
+ --  else GT
+
+instance Num Expense where
+ (+) E {expense = e1,date = d1} E{expense = e2} = default_expense {expense = e1 + e2}
+ (*) E {expense = e1} E{expense = e2} = default_expense {expense = e1 * e2}
+ (-) E {expense = e1} E{expense = e2} = default_expense {expense = e1 - e2}
+ -- abs x = x
+ -- signum x = x
+ fromInteger n = default_expense {expense = (fromInteger n) :: Double}
+ abs E {expense = e1} = if e1 < 0 then default_expense {expense = (-e1)}
+  else default_expense {expense = e1}
+ signum E {expense = e1} = case compare e1 0 of
+  GT -> default_expense {expense = 1}
+  LT -> default_expense {expense = (-1)}
+  EQ -> default_expense {expense = 0}
+ 
+default_expense :: Expense
+default_expense = E (T.pack "1900-01") 0.0 (T.pack "default") (T.pack "default")
+
+-- Define functions, that make Text to [Int,Double]
+text2int :: T.Text -> Int
+text2int x = read ( T.unpack x) :: Int
+
+text2double :: T.Text -> Double
+text2double x = read ( T.unpack x ) :: Double
+
+-- To make an expense from a list of Text
+make_expense :: [T.Text] -> Expense
+make_expense xs = E {
+   date        = xs !! 0
+ , expense     = text2double (xs !! 1)
+ , category    = xs !! 2
+ , description = xs !! 3
+ }
+
+-- Get user input using given prompt.
+get_input :: String -> IO (String)
+get_input prompt = do
+ putStrLn prompt
+ input <- getLine
+ return input
+
+-- Return help value for the implementation.
+print_help :: String --T.Text
+print_help = "-- Help -- \n h - show this help \n a - add new expense \n r - report expenses from given \n q - exit program"
+-- print_help :: IO ()
+-- print_help = putStrLn "-- Help -- \n h - show this help \n a - add new expense \n r - report expenses from given \n q - exit program "
+
+-- validAmount :: String -> IO String
+validAmount prompt = do
+ amount <- get_input prompt
+ let amount_d = readMaybe amount :: Maybe Double
+ if amount_d /= Nothing then
+   return amount
+ else do
+  putStrLn "Invalid number"
+  validAmount prompt
+
+validDate :: String -> IO String
+validDate prompt = do
+ date <- get_input prompt
+ let parts = map T.unpack $ T.splitOn (T.pack "-") $ T.pack date
+ let len_parts = length parts
+ -- if the number of elements is wrong, then ask the date again.
+ if len_parts > 2 || len_parts < 2 then do
+  putStrLn "Invalid date"
+  validDate prompt
+  -- if the number of elements is correct, then check if year and month numbers and month <= 12.
+ else do
+  let year  = checkYear (parts !! 0)
+  let month = checkMonth (parts !! 1)
+  let checkedDate = year ++ "-" ++ month
+  -- if checkedDate is not correct, do ask date again.
+  if checkedDate /= date then do
+   putStrLn "Invalid date"
+   validDate prompt
+  else do
+   return date
+ -- TODO: refactor checkYear and checkMonth
+ where
+  checkYear x = g (readMaybe x :: Maybe Int)
+   where 
+    g Nothing  = "0"
+    g (Just _) = x
+  checkMonth x = g (readMaybe x :: Maybe Int)
+   where 
+    g Nothing  = "0"
+    g (Just y)
+     | y <= 12 && y>0  = x
+     | otherwise       = "0"
+ 
+
+add_exp :: IO String
+add_exp = do
+  date        <- validDate "Date in yyyy-mm format:"
+  expense     <- validAmount "Expense amount:"
+  category    <- get_input "Category of expense:"
+  description <- get_input "Description of expense:"
+  commit      <- get_input "Commit new expense [y/n]?"
+  let new_exp = make_expense $ map T.pack $ [date,expense,category,description]
+  if commit == "y" then do
+   -- TODO: change the state of expenses list
+   print new_exp
+   return "Expense commited"
+  else do
+   print new_exp
+   return "Expense not commited"
+
+
+
+-- Default lower and upper bounds, that are used to create new bounds when reporting.
 lower_Bound :: Expense
 lower_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "lower_bound") (T.pack "lower_bound")
 upper_Bound :: Expense
 upper_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "upper_bound") (T.pack "upper_bound")
 
--- Define ordering by date only.
-instance Ord Expense where
- -- compare E (date _ _ _) (E date2 _ _ _) = if date == date2 then EQ
- -- compare E {date = d1} E{date = d2} = if d1 == d2 then EQ
- --  else if d1 <= d2 then LT
- --  else GT
- (<=) E {date = d1} E{date = d2} = (<=) d1 d2
-
-text2int :: [T.Text] -> Int
-text2int xs = read ( T.unpack ( xs !! 1 )) :: Int
-
-text2double :: [T.Text] -> Double
-text2double xs = read ( T.unpack ( xs !! 1 )) :: Double
-
+-- Parse <10 values as '0<nr>'
 parse_le10 :: Int -> String
 parse_le10 val
- | val < 10 = "0" ++ show val
+ | val < 10  = "0" ++ show val
+ | val > 12  = "01"
  | otherwise = show val
+
+upr_bnd_year_fun :: T.Text -> T.Text -> String
+upr_bnd_year_fun mnth year
+ | mnth > (T.pack "12") = show $ (+1) $ text2int $ year
+ | otherwise            = T.unpack year
+
+calculateAmount :: [Expense] -> Expense
+calculateAmount xs = foldr (+) 0 xs
+
+list_categories :: [Expense] -> [T.Text]
+-- TODO: Kuidas teha see implementation paremini.
+list_categories xs = f [] $ [(category x) | x <- xs]
+ where
+  f zs [] = zs
+  f zs (y:ys) 
+   | not $ (elem) y zs = f (y:zs) ys
+   | otherwise = f zs ys
+
+category_sum :: T.Text -> [Expense] -> Expense
+category_sum x ys = calculateAmount $ filterCat x ys
+ where
+  filterCat x [] = []
+  filterCat x (y:ys)
+   | (==) x (category y) = y : filterCat x ys
+   | otherwise = filterCat x ys
+
+report_exp = do
+ lwr_bnd <- validDate "Select month to report in format yyyy-mm: "
+
+ let lwr_mnth = (!! 1) $ T.splitOn (T.pack "-") $ T.pack lwr_bnd 
+ let lwr_year = (!! 0) $ T.splitOn (T.pack "-") $ T.pack lwr_bnd 
+
+ let upr_bnd_month = parse_le10 $ (+1) $ text2int lwr_mnth
+ let upr_bnd_year = upr_bnd_year_fun lwr_mnth lwr_year
+
+ let lower_bound =  T.pack $ lwr_bnd ++ "-01"
+ let upper_bound = T.pack $ (++) upr_bnd_year $ (++) "-" $ (++) upr_bnd_month "-00"
+ -- TMP
+ content <- parser "data.csv"
+ let expenses_done = map make_expense content
+----------
+ let report =  filter (< upper_Bound {date = upper_bound}) $ filter (>= lower_Bound {date = lower_bound}) expenses_done
+ -- print report
+ putStrLn $ (++) "Total: " $ show $ expense $ calculateAmount report
+ -- get all the categories
+ let report_categories = list_categories report
+ -- print report_categories
+ forM_ report_categories $ \ cat -> putStrLn $ (++) (" -- " ++ (T.unpack cat) ++ ": ") $ show $ expense $ category_sum cat report 
+ return "-----------Report end-----------\n"
+
+   
+loop :: String -> IO String
+loop x = do
+ putStrLn x
+ cmd <- get_input "Insert command: "
+ parse_cmd cmd
+
+parse_cmd :: String -> IO String
+parse_cmd cmd
+ | cmd == "q" = return "Closing the program"
+ | cmd == "h" = loop print_help 
+ | cmd == "a" = add_exp    >>= loop 
+ | cmd == "r" = report_exp >>= loop
+ | otherwise  = loop "Unknown command!"
+
+
+
+--STATE: runState func
+-- expenseState :: [Expense] -> State Int [Expense]
+-- expenseState = runState 
+
 
 -- make list of Expenses
 -- expenses :: IO ([Expense])
-expenses :: IO ()
+expenses :: IO String
 expenses = do
  content <- parser "data.csv"
  let expenses_done = map make_expense content
- print_help
- loop
- -- expenses'
- -- return expenses_done
-  where 
-
-   make_expense :: [T.Text] -> Expense
-   make_expense xs = E {
-      date        = xs !! 0
-    , expense     = text2double xs
-    , category    = xs !! 2
-    , description = xs !! 3
-   }
-
-   get_input :: String -> IO (String)
-   get_input prompt = do
-    putStrLn prompt
-    input <- getLine
-    return input
-
-   print_help :: IO ()
-   print_help = putStrLn "-- Help -- \n h - show this help \n a - add new expense \n r - report expenses from given \n q - exit program "
-   
-   add_exp :: IO ()
-   add_exp = do
-    date        <- get_input "Date in yyyy-mm-dd format:"
-    expense     <- get_input "Expense amount:"
-    category    <- get_input "Category of expense:"
-    description <- get_input "Description of expense:"
-    commit      <- get_input "Commit new expense [y/n]?"
-    let new_exp = make_expense $ map T.pack $ [date,expense,category,description]
-    if commit == "y" then do
-     putStrLn "TODO"
-    else
-     putStrLn "Expense not commited: " >> print new_exp
-
-   report_exp = do
-    -- lower_bound <- get_input
-    lwr_bnd <- get_input "Select month to report in format yyyy-mm: "
-    -- TODO: check user input
-    let lower_bound =  T.pack $ lwr_bnd ++ "-01"
-    let upr_bnd_month = parse_le10 $ (+1) $ text2int $ T.splitOn (T.pack "-") $ T.pack lwr_bnd
-    let upr_bnd_year = T.unpack $ (!! 0) $ T.splitOn (T.pack "-") $ T.pack lwr_bnd
-    let upper_bound = T.pack $ (++) upr_bnd_year $ (++) "-" $ (++) upr_bnd_month "-00"
-    -- TMP
-    content <- parser "data.csv"
-    let expenses_done = map make_expense content
-    -- TODO MAKE NICER
-    let tst =  filter (< upper_Bound {date = upper_bound}) $ filter (>= lower_Bound {date = lower_bound}) expenses_done
-    print tst
-    -- (filter (>= E {date = lower_bound}))
-    -- print lower_bound
-    -- print upper_bound
-
-   parse_cmd :: String -> IO ()
-   parse_cmd cmd
-    | cmd == "q" = putStrLn "Closing the program"
-    | cmd == "h" = print_help                  >> loop
-    | cmd == "a" = add_exp                     >> loop
-    | cmd == "r" = report_exp                  >> loop
-    | otherwise  = putStrLn "Unknown command!" >> loop
-
-   loop :: IO ()
-   loop = do
-    cmd <- get_input "Insert command: "
-    parse_cmd cmd
-
--- control = do
---  exp_list <- expenses
---  -- mapM_ print exp_list
---  let tst = (exp_list !! 0) <= (exp_list !! 1)
---  print tst
+ loop print_help
+ --TODO: write new exp to file
