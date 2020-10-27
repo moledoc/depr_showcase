df982a6093d473ac76a5d5e40e100f8309b6b60c
meelis utt <meelis.utt@gmail.com>
2020-10-27 13:19:37 +0200

    Refactored and added some commets to the code, #7.

--- expenses-hs/Expenses_v3.hs
+++ expenses-hs/Expenses_v3.hs
@@ -1,19 +1,31 @@
 module Expenses_v3 where
 
--- TODOs
--- comment program
--- refactor maybe
-
+----------
+-- imports
 import qualified Data.Text as T
 import Text.Read -- readMaybe
 import Control.Monad -- forM_
 import Control.Monad.State as MS -- State
 import Csv_parser_v2 -- self written simple parser
 
+--------------------
+-- global variables
 dataFile :: String
 dataFile = "data.csv"
 
--- Define new data type Expense
+default_expense :: Expense
+default_expense = E (T.pack "1900-01") 0.0 (T.pack "default") (T.pack "default")
+
+-- Default lower and upper bounds, that are used to create new bounds when reporting.
+lower_Bound :: Expense
+lower_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "lower_bound") (T.pack "lower_bound")
+upper_Bound :: Expense
+upper_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "upper_bound") (T.pack "upper_bound")
+
+----------------------
+-- Defined data type
+-- and it's instances
+
 data Expense = E {
    date        :: T.Text
  , expense     :: Double
@@ -24,10 +36,6 @@ data Expense = E {
 -- Define ordering by date only.
 instance Ord Expense where
  (<=) E {date = d1} E{date = d2} = (<=) d1 d2
- -- compare E (date _ _ _) (E date2 _ _ _) = if date == date2 then EQ
- -- compare E {date = d1} E{date = d2} = if d1 == d2 then EQ
- --  else if d1 <= d2 then LT
- --  else GT
 
 instance Num Expense where
  (+) E {expense = e1,date = d1} E{expense = e2} = default_expense {expense = e1 + e2}
@@ -43,15 +51,15 @@ instance Num Expense where
   LT -> default_expense {expense = (-1)}
   EQ -> default_expense {expense = 0}
  
-default_expense :: Expense
-default_expense = E (T.pack "1900-01") 0.0 (T.pack "default") (T.pack "default")
+-------
+-- MAIN
+main :: IO String
+main = do
+ content <- parser dataFile
+ let state = map make_expense content
+ loop print_help state
 
--- Define functions, that make Text to [Int,Double]
-text2int :: T.Text -> Int
-text2int x = read ( T.unpack x) :: Int
-
-text2double :: T.Text -> Double
-text2double x = read ( T.unpack x ) :: Double
+--HELPER functions
 
 -- To make an expense from a list of Text
 make_expense :: [T.Text] -> Expense
@@ -62,6 +70,32 @@ make_expense xs = E {
  , description = xs !! 3
  }
 
+-- Make T.Text to Double
+text2double :: T.Text -> Double
+text2double x = read ( T.unpack x ) :: Double
+
+--------------------------------------------------
+
+-------
+-- loop
+
+loop :: String -> [Expense] -> IO String
+loop prompt state = do
+ putStrLn prompt
+ cmd <- get_input "Insert command: "
+ parse_cmd cmd state
+
+-- Given user input, do action.
+parse_cmd :: String -> [Expense] -> IO String
+parse_cmd cmd state
+ | cmd == "q" = return "Closing the program"
+ | cmd == "h" = loop print_help state
+ | cmd == "a" = add_exp    state >>= (\(prompt,state) -> loop prompt state)
+ | cmd == "r" = report_exp state >>= (\prompt -> loop prompt state)
+ | otherwise  = loop "Unknown command!" state
+
+-- HELPER functions
+
 -- Get user input using given prompt.
 get_input :: String -> IO (String)
 get_input prompt = do
@@ -69,70 +103,16 @@ get_input prompt = do
  input <- getLine
  return input
 
--- Return help value for the implementation.
+--------------------------------------------------
+
+---------------
+-- ACTION: help
+
 print_help :: String
 print_help = "-- Help -- \n h - show this help \n a - add new expense \n r - report expenses from given \n q - exit program"
 
--- validAmount :: String -> IO String
-validAmount prompt = do
- amount <- get_input prompt
- let amount_d = readMaybe amount :: Maybe Double
- if amount_d /= Nothing then
-   return amount
- else do
-  putStrLn "Invalid number"
-  validAmount prompt
-
- -- TODO: refactor checkYear and checkMonth
-checkYear :: String -> String
-checkYear x = g (readMaybe x :: Maybe Int)
- where 
-  g Nothing  = "0"
-  g (Just _) = x
-
-checkMonth :: String -> String
-checkMonth x = g (readMaybe x :: Maybe Int)
- where 
-  g Nothing  = "0"
-  g (Just y)
-   | y <= 12 && y>0  = x
-   | otherwise       = "0"
-
-checkDay :: String -> String -> String -> String
-checkDay day month year = g (readMaybe day :: Maybe Int)
- where
-  g Nothing = "0"
-  g (Just y)
-   | (elem) month ["01","03","05","07","08","10","12"] && y <= 31      = day
-   | (elem) month ["04","06","09","11"] && y <= 30                     = day
-   | (elem) month ["02"] && y <= 29 && (mod) (read year :: Int) 4 == 0 = day
-   | (elem) month ["02"] && y < 29  && (mod) (read year :: Int) 4 /= 0 = day
-   | otherwise                                                         = "0"
-
-checkDate :: [String] -> Int -> String
--- checkDate = undefined
-checkDate parts 2 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1))
-checkDate parts 3 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1)) ++ "-" ++ (checkDay (parts !! 2) (parts !! 1) (parts !! 0))
-
-
-validDate :: String -> Int -> IO String
-validDate prompt n = do
- date <- get_input prompt
- let parts = map T.unpack $ T.splitOn (T.pack "-") $ T.pack date
- let len_parts = length parts
- -- if the number of elements is wrong, then ask the date again.
- if len_parts > n || len_parts < n then do
-  putStrLn "Invalid date"
-  validDate prompt n
- else do
-  -- if checkDate is not correct, ask date again.
-  if (checkDate parts n) /= date then do
-   putStrLn "Invalid date"
-   validDate prompt n
-  -- else return date
-  else
-   return date
- 
+--------------
+-- ACTION: add
 
 add_exp :: [Expense] -> IO (String, [Expense])
 add_exp state = do
@@ -154,44 +134,78 @@ add_exp state = do
    addComas x [] = x ++ "\n"
    addComas x y  = x ++ "," ++ y
 
+-- HELPER functions
 
+---- validAmount
+-- get amount input from user.
+validAmount :: String -> IO String
+validAmount prompt = do
+ amount <- get_input prompt
+ let amount_d = readMaybe amount :: Maybe Double
+ if amount_d /= Nothing then
+   return amount
+ else do
+  putStrLn "Invalid number"
+  validAmount prompt
 
--- Default lower and upper bounds, that are used to create new bounds when reporting.
-lower_Bound :: Expense
-lower_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "lower_bound") (T.pack "lower_bound")
-upper_Bound :: Expense
-upper_Bound = E (T.pack "2020-01-01") 0.0 (T.pack "upper_bound") (T.pack "upper_bound")
+----- validDate
+-- get date value from user
+validDate :: String -> Int -> IO String
+validDate prompt n = do
+ date <- get_input prompt
+ let parts = map T.unpack $ T.splitOn (T.pack "-") $ T.pack date
+ let len_parts = length parts
+ -- if the number of elements is wrong, then ask the date again.
+ if len_parts > n || len_parts < n then do
+  putStrLn "Invalid date"
+  validDate prompt n
+ else do
+  -- if checkDate is not correct, ask date again.
+  if (checkDate parts n) /= date then do
+   putStrLn "Invalid date"
+   validDate prompt n
+  else
+   return date
 
--- Parse <10 values as '0<nr>'
-parse_le10 :: Int -> String
-parse_le10 val
- | val < 10  = "0" ++ show val
- | val > 12  = "01"
- | otherwise = show val
+-- check validity of given date
+-- either for format yyyy-mm or yyyy-mm-dd.
+checkDate :: [String] -> Int -> String
+checkDate parts 2 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1))
+checkDate parts 3 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1)) ++ "-" ++ (checkDay (parts !! 2) (parts !! 1) (parts !! 0))
 
-upr_bnd_year_fun :: T.Text -> T.Text -> String
-upr_bnd_year_fun mnth year
- | mnth > (T.pack "12") = show $ (+1) $ text2int $ year
- | otherwise            = T.unpack year
+-- Check, if given year is valid and in wanted format.
+checkYear :: String -> String
+checkYear x = g (readMaybe x :: Maybe Int)
+ where 
+  g Nothing  = "0"
+  g (Just _) = x
 
-calculateAmount :: [Expense] -> Expense
-calculateAmount xs = foldr (+) 0 xs
+-- Check, if given month is valid and in wanted format.
+checkMonth :: String -> String
+checkMonth x = g (readMaybe x :: Maybe Int)
+ where 
+  g Nothing  = "0"
+  g (Just y)
+   | y <= 12 && y>0  = x
+   | otherwise       = "0"
 
-list_categories :: [Expense] -> [T.Text]
-list_categories xs = f [] $ [(category x) | x <- xs]
+-- Check if given day is a valid day given month (and leap/non-leap year)
+-- and in wanted format.
+checkDay :: String -> String -> String -> String
+checkDay day month year = g (readMaybe day :: Maybe Int)
  where
-  f zs [] = zs
-  f zs (y:ys) 
-   | not $ (elem) y zs = f (y:zs) ys
-   | otherwise = f zs ys
+  g Nothing = "0"
+  g (Just y)
+   | (elem) month ["01","03","05","07","08","10","12"] && y <= 31      = day
+   | (elem) month ["04","06","09","11"] && y <= 30                     = day
+   | (elem) month ["02"] && y <= 29 && (mod) (read year :: Int) 4 == 0 = day
+   | (elem) month ["02"] && y < 29  && (mod) (read year :: Int) 4 /= 0 = day
+   | otherwise                                                         = "0"
 
-category_sum :: T.Text -> [Expense] -> Expense
-category_sum x ys = calculateAmount $ filterCat x ys
- where
-  filterCat x [] = []
-  filterCat x (y:ys)
-   | (==) x (category y) = y : filterCat x ys
-   | otherwise = filterCat x ys
+--------------------------------------------------
+
+----------------
+--ACTION: report
 
 report_exp :: [Expense] -> IO String
 report_exp state = do
@@ -216,42 +230,46 @@ report_exp state = do
  forM_ report_categories $ \ cat -> putStrLn $ (++) (" -- " ++ (T.unpack cat) ++ ": ") $ show $ expense $ category_sum cat report 
  return "-----------Report end-----------\n"
 
-   
-loop :: String -> [Expense] -> IO String
-loop prompt state = do
- putStrLn prompt
- cmd <- get_input "Insert command: "
- parse_cmd cmd state
+-- HELPER functions
 
-parse_cmd :: String -> [Expense] -> IO String
-parse_cmd cmd state
- | cmd == "q" = return "Closing the program"
- | cmd == "h" = loop print_help state
- | cmd == "a" = add_exp    state >>= (\(prompt,state) -> loop prompt state)
- | cmd == "r" = report_exp state >>= (\prompt -> loop prompt state)
- | otherwise  = loop "Unknown command!" state
+-- Make T.Text to Int
+text2int :: T.Text -> Int
+text2int x = read ( T.unpack x) :: Int
 
+-- Parse <10 values as '0<nr>'
+parse_le10 :: Int -> String
+parse_le10 val
+ | val < 10  = "0" ++ show val
+ | val > 12  = "01"
+ | otherwise = show val
 
+-- If lower bound month is 12 (december), then add +1 to year number
+upr_bnd_year_fun :: T.Text -> T.Text -> String
+upr_bnd_year_fun mnth year
+ | mnth > (T.pack "12") = show $ (+1) $ text2int $ year
+ | otherwise            = T.unpack year
 
-----STATE: runState func
---expenseState :: Expense -> State [Expense] [ Expense ]
---expenseState new = do
--- cur <- MS.get
--- put (new:cur)
--- return cur 
- 
---getExpenseState cur = do
--- cur <- MS.get
--- return cur
+-- Calculate total expense amount of given list of Expenses.
+calculateAmount :: [Expense] -> Expense
+calculateAmount xs = foldr (+) 0 xs
 
- -- return cur
--- expenseState :: State Int [Expense]
--- expenseState = runState [Expense] []
+-- List unique categories.
+list_categories :: [Expense] -> [T.Text]
+-- TODO: how to make unique list more elegantly?
+list_categories xs = f [] $ [(category x) | x <- xs]
+ where
+  f zs [] = zs
+  f zs (y:ys) 
+   | not $ (elem) y zs = f (y:zs) ys
+   | otherwise = f zs ys
 
+-- Calculate total amount of given Expenses list by given category.
+category_sum :: T.Text -> [Expense] -> Expense
+category_sum x ys = calculateAmount $ filterCat x ys
+ where
+  filterCat x [] = []
+  filterCat x (y:ys)
+   | (==) x (category y) = y : filterCat x ys
+   | otherwise = filterCat x ys
 
--- make list of Expenses
--- expenses :: IO String
-expenses = do
- content <- parser dataFile
- let state = map make_expense content
- loop print_help state
+--------------------------------------------------
