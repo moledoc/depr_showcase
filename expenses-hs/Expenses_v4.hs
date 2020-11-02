4cbc2d5c4a056c2281e821cd023f7182b90b8566
meelis utt <meelis.utt@gmail.com>
2020-11-02 19:41:35 +0200

    moved stuff to depr/; new version of expenses-hs

--- expenses-hs/Expenses_v4.hs
+++ expenses-hs/Expenses_v4.hs
@@ -1,12 +1,12 @@
 module Expenses where
 
 -- IMPORTS
-
 import Control.Exception
 import qualified Data.Text as T --hoogle Data.Text 2020-10-10: This module is intended to be imported qualified, to avoid name clashes with Prelude functions
 import qualified Data.Text.IO as T -- use strict (instead of lazy) readFile.
 import Text.Read -- readMaybe
 import Control.Monad --forM_
+import Data.List -- nub
 
 -- DEFINITIONS
 dataFile :: FilePath
@@ -25,20 +25,21 @@ data Expense = E {
 instance Show Expense where
  show (E da exp cat de) = show da ++ "," ++ show exp ++ "," ++ (T.unpack cat) ++ "," ++ (T.unpack de) ++ "\n"
 
+instance Ord Expense where
+ (<=) (E {expense = e1}) (E {expense = e2}) = e1 <= e2
+
 data Date = D {
    year  :: Int
  , month :: Int
  , day   :: Int
-} deriving (Eq)
+} deriving (Eq,Ord)
 
 instance Show Date where
- -- show (D y m Nothing)  = show y ++ "-" ++ show m
- -- show (D y m (Just d)) = show y ++ "-" ++ show m ++ "-" ++ show d
  show (D y m d) = show y ++ "-" ++ (lt10 m) ++ show m ++ "-" ++ (lt10 d) ++ show d
 
--- MAIN FUNCTIONS
 
-main :: IO String
+-- MAIN FUNCTIONS
+main :: IO ()
 main = do
  rawcontent <- readData
  parsedcontent <- parseContent $ T.lines rawcontent
@@ -57,45 +58,45 @@ parseContent contents = do
  let body   = getBody contents
  return (header,body)
  where
-  getBody b = map makeExpense $ map (T.splitOn $ T.pack ",") $ filter (\x -> (/=) (T.pack "") $ T.take 1 x) $ tail b
+  getBody b = map makeExpenseText $ map (T.splitOn $ T.pack ",") $ filter (\x -> (/=) (T.pack "") $ T.take 1 x) $ tail b
 
 
-loop :: String -> (T.Text,[Expense]) -> IO String
+loop :: String -> (T.Text,[Expense]) -> IO ()
 loop prompt state = do
  putStrLn prompt
  cmd <- getInput "Insert command: "
  parseCMD cmd state
 
-parseCMD :: String -> (T.Text,[Expense]) -> IO String
+parseCMD :: String -> (T.Text,[Expense]) -> IO ()
 parseCMD cmd state
- | cmd == "q"  = return "Closing the program"
+ | cmd == "q"  = putStrLn "Closing the program"
  | cmd == "h"  = loop printHelp state
- -- | cmd == "a"  = addExp     state >>= (\(prompt,state) -> loop prompt state)
- | cmd == "rs" = reportShow state >>= (\prompt -> loop prompt state)
- -- | cmd == "r" = reportExp state >>= (\prompt -> loop prompt state)
- | cmd == "d" = delExp    state   >>= (\(prompt,state) -> loop prompt state)
+ | cmd == "a"  = addExp            state >>= (\(prompt,state) -> loop prompt state)
+ | cmd == "d" = delExp             state >>= (\(prompt,state) -> loop prompt state)
+ | cmd == "rl" = reportShow        state >>= (\prompt -> loop prompt state)
+ | cmd == "rr" = reportRange       state >>= (\prompt -> loop prompt state)
+ | cmd == "rn" = reportAfterParams state >>= (\prompt -> loop prompt state)
+ | cmd == "rc" = reportCategory    state >>= (\prompt -> loop prompt state)
  | otherwise  = loop "Unknown command!" state
 
 printHelp :: String
-printHelp = "-- Help -- \n h - show this help \n a - add new expense \n rs - show last inserted expenses \n r - report expenses \n d - delete expense \n q - exit program"
+printHelp = "-- Help -- \n h  - show this help \n a  - add new expense \n d  - delete expense \n rl - show last inserted expenses \n rr - show expenses between range \n rn - report <n> month expenses \n rc - report expenses of chosen category \n q  - exit program"
 
--- addExp :: (T.Text,[Expense]) -> IO (String, (T.Text,[Expense]))
--- addExp (header,body) = do
---  date        <- validDate "Date in yyyy-mm-dd format:" 3
---  expense     <- validAmountD "Expense amount:"
---  category    <- T.pack $ getInput "Category of expense:"
---  description <- T.pack $ getInput "Description of expense:"
---  -- let newexp = makeExpense $ map T.pack [date,expense,category,description]
---  let newexp = E date expense category description
---  let newbody = newexp : body
---  appendFile dataFile (show newexp)
---  -- return ("Expense added: " ++ show newexp,(header,newexp : body))
---  return ("Expense added: " ++ show newexp,(header:newbody))
+addExp :: (T.Text,[Expense]) -> IO (String, (T.Text,[Expense]))
+addExp (header,body) = do
+ date        <- validDate "Date in yyyy-mm-dd format: " 3
+ expense     <- validAmountD "Expense amount: "
+ category    <- getInput "Category of expense: "
+ description <- getInput "Description of expense: "
+ let newexp = E date expense (T.pack category) (T.pack description)
+ let newbody = newexp : body
+ appendFile dataFile (show newexp)
+ return ("Expense added: " ++ show newexp,(header,newbody))
 
 delExp :: (T.Text,[Expense]) -> IO (String, (T.Text,[Expense]))
 delExp (header,body) = do
  (nrOfExp,selection) <- showLastNExpenses body
- nrOfDel <- validAmountI "Delete <nr> expense: "
+ nrOfDel <- validAmountI "Delete expense nr: "
  if nrOfDel >= nrOfExp  || nrOfDel < 0 then do
   putStrLn "Invalid number"
   delExp (header,body)
@@ -109,20 +110,56 @@ delExp (header,body) = do
 reportShow :: (T.Text,[Expense]) -> IO String
 reportShow (header,body) = do
  (nrOfExp,selection) <- showLastNExpenses body
- return $ "End of last " ++ show nrOfExp ++ "expenses inserted"
+ return $ "Last " ++ show nrOfExp ++ " inserted expenses shown"
 
--- reportRange :: (T.Text,[Expense]) -> IO (String, (T.Text,[Expense]))
--- reportRange (header,body) = do
---  start <- validDate "Select start date in yyyy-mm-dd format:" 3
---  end   <- validDate "Select end date in yyyy-mm-dd format:"   3
---  if start>end then do
---   putStr "Invalid start date"
---   reportRange (header,body)
---  else do
---   let lowerbound = makeDate start
---   let upperbound = makeDate end
---   print "tmp"
+reportRange :: (T.Text,[Expense]) -> IO String
+reportRange (header,body) = do
+ start <- validDate "Select start date in yyyy-mm-dd format: " 3
+ end   <- validDate "Select end date in yyyy-mm-dd format: "   3
+ if start>end then do
+  putStrLn "Invalid start date"
+  reportRange (header,body)
+ else do
+  showRangeExpenses start end body
+  return $ "Expenses between [" ++ show start ++ ", " ++ show end ++ ") shown"
 
+reportAfterParams :: (T.Text,[Expense]) -> IO String
+reportAfterParams state = do
+ format <- validAmountI "Report with month accuracy (1) or date accuracy (2): "
+ if (not $ (elem) format [1,2]) then do
+  putStrLn "Invalid format!"
+  reportAfterParams state
+ else do
+   n <- validAmountI "Number of months to be reported: "
+   if n < 1 then do
+    putStrLn "Invalid number of months!"
+    reportAfterParams state
+   else do
+    if format == 1 then
+     reportAfter state n (format+1) ""
+    else
+     reportAfter state n (format+1) "-dd"
+     
+reportAfter :: (T.Text,[Expense]) -> Int -> Int -> String -> IO String
+reportAfter (header,body) n formatI formatS = do
+ start <- validDate ("Select month in yyyy-mm"++ formatS ++ " format: ") formatI
+ let end = dateAfter start n
+ showRangeExpenses start end body
+ return $ "Expenses between [" ++ show start ++ ", " ++ show end ++ ") shown"
+
+reportCategory (header,body) = do
+ report_init <- listValues categoryList body "category" returnCategoryExpenses
+ selectDescription <- getInput "Select description as well? [y/n]: "
+ filterDesc <- reportDescription selectDescription report_init
+ let report = reverse $ sort filterDesc 
+ mapM_ putStr $ map show report
+ return "Expenses with category (and desctionrion) shown"
+
+reportDescription :: String -> [Expense] -> IO [Expense]
+reportDescription "y" body = listValues descriptionList body "description" returnDescriptionExpenses
+reportDescription "n" body = return body
+reportDescription _ body = putStrLn "Invalid option" >> getInput "Select description as well? [y/n]: " >>= (\opt -> reportDescription opt body) 
+  
 
 -- HELP FUNCTIONS
 
@@ -133,13 +170,14 @@ lt10 n
 
 getInput :: String -> IO (String)
 getInput prompt = do
- putStrLn prompt
+ -- putStrLn $ "\n" ++ prompt
+ putStr prompt
  input <- getLine
  return input
 
-makeExpense :: [T.Text] -> Expense
-makeExpense xs = E{
-   date        = makeDate (xs !! 0)
+makeExpenseText :: [T.Text] -> Expense
+makeExpenseText xs = E{
+   date        = makeDateText (xs !! 0)
  , expense     = text2Double (xs !! 1)
  , category    = xs !! 2
  , description = xs !! 3
@@ -148,8 +186,15 @@ makeExpense xs = E{
 text2Double :: T.Text -> Double
 text2Double x = read ( T.unpack x ) :: Double
 
-makeDate :: T.Text -> Date
-makeDate date = D {
+makeDateInt :: [Int] -> Date
+makeDateInt xs = D {
+   year  = xs !! 0
+ , month = xs !! 1
+ , day   = xs !! 2
+}
+
+makeDateText :: T.Text -> Date
+makeDateText date = D {
     year  = text2Int $ (split 0 date)
   , month = text2Int $ (split 1 date)
   , day   = text2Int $ (split 2 date)
@@ -160,31 +205,83 @@ makeDate date = D {
 text2Int :: T.Text -> Int
 text2Int x = read ( T.unpack x) :: Int
 
+
+listValues fsel body what ffil= do
+ let selection = fsel body
+ let nrOfSel = [1..(length selection)]
+ mapM_ (\x -> putStrLn $ show x ++ ": " ++ T.unpack ((!! (x-1)) selection)) nrOfSel
+ nr <- validAmountI ("Number of "++ what ++ " to be reported: ")
+ if nr < 0 || nr > (length selection) then do
+  putStrLn "Invalid number"
+  listValues fsel body what ffil
+ else
+  return $ ffil (selection !! (nr-1)) body
+
 showLastNExpenses :: [Expense] -> IO (Int, [Expense])
 showLastNExpenses body = do
- nr <- validAmountI "Show last <nr> inserted expenses:"
+ nr <- validAmountI "Show last <nr> inserted expenses: "
  let nrOfExp = min (length body) nr
- let selection = (take nrOfExp [0..],take nrOfExp body)
- mapM_ (\x -> putStr $ show x ++ ": " ++ show ( (!! x) $ snd selection)) (fst selection)
+ let selection = (take nrOfExp [1..],take nrOfExp body)
+ mapM_ (\x -> putStr $ show x ++ ": " ++ show ( (!! (x-1)) $ snd selection)) (fst selection)
  return (nrOfExp,snd selection)
+
+
+returnDescriptionExpenses :: T.Text -> [Expense] -> [Expense]
+returnDescriptionExpenses desc body = filter f body
+ where
+  f exp = g desc exp
+  g cat (E {description = desc1})
+   | desc == desc1 = True
+   | otherwise     = False
+
+returnCategoryExpenses :: T.Text -> [Expense] -> [Expense]
+returnCategoryExpenses cat body = filter f body
+ where
+  f exp = g cat exp
+  g cat (E {category = cat1})
+   -- | (T.toUpper cat) == (T.toUpper cat1) = True
+   | cat == cat1 = True
+   | otherwise   = False
+
+returnRangeExpenses :: Date -> Date -> [Expense] -> [Expense]
+returnRangeExpenses start end body = filter f body
+ where
+  f exp = g start end exp
+  g start end (E {date = d1})
+   | start <= d1 && d1 < end = True
+   | otherwise               = False
+
+showRangeExpenses :: Date -> Date -> [Expense] -> IO ()
+showRangeExpenses start end body = do
+  let report = returnRangeExpenses start end body
+  let categorylist = categoryList report
+  putStrLn $ (++) "Total: " $ show $ calculateExpense report
+  -- forM_ categorylist $ (\cat -> putStrLn $ (++) (" -- " ++ (T.unpack cat) ++ ": ") $ show $ calculateExpense $ filter (\ E {category = xcat} -> xcat == cat) report)
+  forM_ categorylist $ (\cat -> putStrLn $ (++) (" -- " ++ (T.unpack cat) ++ ": ") $ show $ calculateExpense $ returnCategoryExpenses cat report)
+
+calculateExpense :: [Expense] -> Double
+calculateExpense xs = foldr (\E {expense = exp} acc -> exp + acc) 0 xs
+
+categoryList :: [Expense] -> [T.Text]
+categoryList xs = nub [(category x) | x <- xs]
+
+descriptionList :: [Expense] -> [T.Text]
+descriptionList xs = nub [(description x) | x <- xs]
 
 isValidDouble :: String -> Maybe Double
 isValidDouble x = readMaybe x :: Maybe Double
+
 isValidInt :: String -> Maybe Int
 isValidInt x = readMaybe x :: Maybe Int
 
 validAmountLoop f prompt Nothing  = putStrLn "Invalid number" >> f prompt
 validAmountLoop f prompt (Just x) = return x
 
-validAmountStatic Nothing  = Nothing
-validAmountStatic (Just x) = x
-
 validAmountD :: String -> IO Double
 validAmountD prompt = do
  amount <- getInput prompt
  let maybedouble = isValidDouble amount
  validAmountLoop validAmountD prompt maybedouble
-
 
 validAmountI :: String -> IO Int
 validAmountI prompt = do
@@ -193,62 +290,70 @@ validAmountI prompt = do
  let maybedouble = isValidDouble amount
  validAmountLoop validAmountI prompt maybeint
 
-
-
--- validDate :: String -> Int -> IO Date
+validDate :: String -> Int -> IO Date
 validDate prompt n = do
  date <- getInput prompt
  let parts = map T.unpack $ T.splitOn (T.pack "-") $ T.pack date
- -- print parts
- -- forM_ areValidNumber $ (\p -> print $ show p)
- let areValidNumbers = map isValidInt parts
- print areValidNumbers
- print ""
- -- let partslength = length parts
- -- -- if the number of elements is wrong, then ask the date again.
- -- if partslength /= n then do
- --  putStrLn "Invalid date"
- --  validDate prompt n
- -- else do
- --  -- if checkDate is not correct, ask date again.
- --  if (checkDate parts n) /= date then do
- --   putStrLn "Invalid date"
- --   validDate prompt n
- --  else
- --   return date
+ if (length parts) /= n then do
+  putStrLn "Invalid Date"
+  validDate prompt n
+ else do
+   areValidNumbers <- yyyymmddDate $ map isValidInt parts
+   isValidDate <- checkDate areValidNumbers
+   if (not isValidDate) then do
+    putStrLn "Invalid Date"
+    validDate prompt n
+   else do
+    let validDateNumbers = map (\(Just x) -> x) areValidNumbers
+    return $ makeDateInt validDateNumbers
+   
+yyyymmddDate :: [Maybe Int] -> IO [Maybe Int]
+yyyymmddDate date = do
+ if ( length date ) == 2 then do
+  return $ date ++ [ Just 1 ]
+ else do
+  return date
 
--- check validity of given date
--- either for format yyyy-mm or yyyy-mm-dd.
-checkDate :: [String] -> Int -> String
-checkDate parts 2 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1))
-checkDate parts 3 = (checkYear (parts !! 0)) ++ "-" ++ (checkMonth (parts !! 1)) ++ "-" ++ (checkDay (parts !! 2) (parts !! 1) (parts !! 0))
+checkDate :: [Maybe Int] -> IO Bool
+checkDate xs = do
+ let isValidYear  = checkYear  (xs !! 0)
+ let isValidMonth = checkMonth (xs !! 1)
+ let isValidDay   = checkDay   (xs !! 0) (xs !! 1) (xs !! 2)
+ if (isValidYear && isValidMonth && isValidDay) then do
+  return True
+ else do
+  return False
 
--- Check, if given year is valid and in wanted format.
-checkYear :: String -> String
-checkYear x = g (readMaybe x :: Maybe Int)
- where 
-  g Nothing  = "0"
-  g (Just _) = x
+checkYear :: Maybe Int -> Bool
+checkYear Nothing = False
+checkYear (Just year)
+ | year > 1900 = True
+ | otherwise   = False
 
--- Check, if given month is valid and in wanted format.
-checkMonth :: String -> String
-checkMonth x = g (readMaybe x :: Maybe Int)
- where 
-  g Nothing  = "0"
-  g (Just y)
-   | y <= 12 && y>0  = x
-   | otherwise       = "0"
+checkMonth :: Maybe Int -> Bool
+checkMonth Nothing = False
+checkMonth (Just month)
+ | month <=12 && month > 0 = True
+ | otherwise = False
 
--- Check if given day is a valid day given month (and leap/non-leap year)
--- and in wanted format.
-checkDay :: String -> String -> String -> String
-checkDay day month year = g (readMaybe day :: Maybe Int)
- where
-  g Nothing = "0"
-  g (Just y)
-   | (elem) month ["01","03","05","07","08","10","12"] && y <= 31      = day
-   | (elem) month ["04","06","09","11"] && y <= 30                     = day
-   | (elem) month ["02"] && y <= 29 && (mod) (read year :: Int) 4 == 0 = day
-   | (elem) month ["02"] && y < 29  && (mod) (read year :: Int) 4 /= 0 = day
-   | otherwise                                                         = "0"
+checkDay :: Maybe Int -> Maybe Int -> Maybe Int -> Bool
+checkDay Nothing _ _ = False
+checkDay _ Nothing _ = False
+checkDay _ _ Nothing = False
+checkDay (Just year) (Just month) (Just day)
+ | day <= 0                                           = False
+ | (elem) month [1,3,5,7,8,10,12] && day <= 31        = True
+ | (elem) month [4,6,9,11] && day <= 30               = True
+ | (elem) month [2] && day <= 29 && (mod) year 4 == 0 = True
+ | (elem) month [2] && day < 29  && (mod) year 4 /= 0 = True
+ | otherwise                                          = False
 
+dateAfter :: Date -> Int -> Date
+dateAfter (D {year = y, month = m, day = d}) n = do
+ let month = (mod) (m+n) 12
+ let year = (+) y $ max 0 $ (div) (m+n-1) 12
+ if month == 0 then
+  makeDateInt [year,month+12,d]
+ else
+  makeDateInt [year,month,d]
+  
