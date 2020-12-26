d4cbb72ba4835577b729aad59afbc199b5642ae3
meelis utt <meelis.utt@gmail.com>
2020-12-26 13:39:32 +0200

    init makeReport function

--- expenses-scala/Expenses.scala
+++ expenses-scala/Expenses.scala
@@ -1,19 +1,55 @@
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
-object Expense{
-    var date = "1900-01-01"
-    var expense = 0
-    var type = "default"
-    var description = "default"
-}
+object Expenses{
 
+    class Expense(d:String,e:Double,sec:String,desc:String){
+        var date = d
+        var expense = e
+        var section = sec
+        var description = desc
 
-def main(args: Array[String]): Unit = {
-    println("test")
-    // val bufferedSource = Source.fromFile("data.csv")
-    // for (line <- bufferedSource.getLines) {
-    //     println(line.toUpperCase)
-    // }
+        // def toString(exp: Expense): String = {
+        //     exp.date ++ exp.expense.toString ++ exp.section ++ exp.description
+        // }
+    }
 
-    // bufferedSource.close
+    def makeExpense(line: String): Expense = {
+        val elements = line.split(",")
+        new Expense(elements(0),elements(1).toDouble,elements(2),elements(3))
+    }
+    
+    def expenseSum(exp: ListBuffer[Expense]): Double = {
+        exp.foldLeft(0.0)((acc,x)=>acc+x.expense)
+    }
+
+    def makeReport(exp: ListBuffer[Expense]): Unit = {
+        // get user input for start and end date
+        val start = scala.io.StdIn.readLine("Start date: ")
+        val end = scala.io.StdIn.readLine("End date: ")
+        val expensesList = exp.filter(x => x.date>=start && x.date<end)
+
+        // calculate total
+        println("TOTAL: " ++ expenseSum(expensesList).toString)
+
+        // calculate by section
+        expensesList.groupBy(_.section).foreach(x=>println(" -- " ++ x._1 ++ ": " ++ expenseSum(x._2).toString))
+    }
+
+    def main(args: Array[String]): Unit = {
+        var expensesList = new ListBuffer[Expense]()
+
+        // read in file and make expenses
+        val bufferedSource = Source.fromFile("data.csv")
+        for (line <- bufferedSource.getLines) {
+            if (!line.toUpperCase.contains("EXPENSE")){
+                expensesList += makeExpense(line.toUpperCase)
+            }
+        }
+        bufferedSource.close
+
+        // // Report
+        // makeReport(expensesList)
+        
+    }
 }
