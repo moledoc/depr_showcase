a49e7b132525ef1c178b320cb915aa3e71a69344
molelinux <meelis.utt@gmail.com>
2020-12-26 11:55:54 +0200

    expenses-scala init push

---
+++ expenses-scala/Expenses.scala
@@ -0,0 +1,19 @@
+import scala.io.Source
+
+object Expense{
+    var date = "1900-01-01"
+    var expense = 0
+    var type = "default"
+    var description = "default"
+}
+
+
+def main(args: Array[String]): Unit = {
+    println("test")
+    // val bufferedSource = Source.fromFile("data.csv")
+    // for (line <- bufferedSource.getLines) {
+    //     println(line.toUpperCase)
+    // }
+
+    // bufferedSource.close
+}
