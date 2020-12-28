e913d8837e50f9e8ea5ff0b2604d83ac8bf4eee5
molelinux <meelis.utt@gmail.com>
2020-12-28 17:41:49 +0200

    buttons and text areas work; TODO: implement value checks

--- expenses-scala/src/main/scala/Expenses.scala
+++ expenses-scala/src/main/scala/Expenses.scala
@@ -12,13 +12,16 @@ import scalafx.scene.control.{Button, DateCell, DatePicker, MenuButton, ScrollPa
 import scalafx.scene.layout.{BorderPane, HBox, TilePane, VBox}
 
 import java.util.Calendar
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 object Expenses extends JFXApp {
   val datafile = "dat.csv"
   val buttonWidth = 125
+  var header = ""
 
-  class Expense(d:String,e:Double,sec:String,desc:String){
+  class Expense(d: String, e: Double, sec: String, desc: String) {
     var date = d
     var expense = e
     var section = sec
@@ -31,33 +34,33 @@ object Expenses extends JFXApp {
 
   def makeExpense(line: String): Expense = {
     val elements = line.split(",")
-    new Expense(elements(0),elements(1).toDouble,elements(2),elements(3))
+    new Expense(elements(0), elements(1).toDouble, elements(2), elements(3))
   }
 
   def expenseSum(exp: List[Expense]): Double = {
-    exp.foldLeft(0.0)((acc,x)=>acc+x.expense)
+    exp.foldLeft(0.0)((acc, x) => acc + x.expense)
   }
 
-  def makeReport(exp:List[Expense],start: String,end: String): String ={
-//    // get user input for start and end date
-//    val start = scala.io.StdIn.readLine("Start date: ")
-//    val end = scala.io.StdIn.readLine("End date: ")
-    val expensesList = exp.filter(x => x.date>=start && x.date<end)
+  def makeReport(exp: List[Expense], start: String, end: String): String = {
+    //    // get user input for start and end date
+    //    val start = scala.io.StdIn.readLine("Start date: ")
+    //    val end = scala.io.StdIn.readLine("End date: ")
+    val expensesList = exp.filter(x => x.date >= start && x.date < end)
     // calculate report
-    val output = expensesList.groupBy(_.section).foldRight(start ++ " -- " ++ end ++ "\n" ++ "TOTAL: " ++ expenseSum(expensesList).toString)((x,acc)=>acc ++ "\n -- " ++ x._1 ++ ": " ++ expenseSum(x._2).toString)
+    val output = expensesList.groupBy(_.section).foldRight(start ++ " -- " ++ end ++ "\n" ++ "TOTAL: " ++ expenseSum(expensesList).toString)((x, acc) => acc ++ "\n -- " ++ x._1 ++ ": " ++ expenseSum(x._2).toString)
     output
   }
 
-  def addExpense(exp:Map[Int,Expense],date:String,expense:Double,section:String,description:String): Map[Int,Expense] = {
-//    val date = scala.io.StdIn.readLine("Date: ")
-//    val expense = scala.io.StdIn.readLine("Amount: ").toDouble
-//    val section = scala.io.StdIn.readLine("Type: ")
-//    val description = scala.io.StdIn.readLine("Description: ")
-    val newExp = new Expense(date,expense,section,description)
+  def addExpense(exp: Map[Int, Expense], date: String, expense: Double, section: String, description: String): Map[Int, Expense] = {
+    //    val date = scala.io.StdIn.readLine("Date: ")
+    //    val expense = scala.io.StdIn.readLine("Amount: ").toDouble
+    //    val section = scala.io.StdIn.readLine("Type: ")
+    //    val description = scala.io.StdIn.readLine("Description: ")
+    val newExp = new Expense(date, expense, section, description)
 
     val fw = new FileWriter(datafile, true)
     try {
-      fw.write(newExp.toString++"\n")
+      fw.write(newExp.toString ++ "\n")
     }
     finally fw.close()
 
@@ -66,42 +69,48 @@ object Expenses extends JFXApp {
     addNew
   }
 
-  def rmExpense(exp:Map[Int,Expense],rmIdx: Int): Map[Int,Expense] = {
-    var rmExp = ListMap(exp.toSeq.sortBy(_._1):_*)
+  def rmExpense(exp: Map[Int, Expense], rmIdx: Int): Map[Int, Expense] = {
+    var rmExp = ListMap(exp.toSeq.sortBy(_._1): _*)
     // TODO: printida
-//    val rmIdx = scala.io.StdIn.readLine("Select the index of expense that you want to delete: ").toInt
-    rmExp-=rmIdx
+    //    val rmIdx = scala.io.StdIn.readLine("Select the index of expense that you want to delete: ").toInt
+    rmExp -= rmIdx
 
+    val pw = new PrintWriter(datafile)
+    pw.write(header)
+    pw.close()
     val fw = new FileWriter(datafile, true)
     try {
-      rmExp.foreach(x=>fw.write(x._2.toString++"\n"))
+      rmExp.foreach(x => fw.write(x._2.toString ++ "\n"))
     }
     finally fw.close()
 
     rmExp
   }
 
-  def printExpenses(exp: Map[Int,Expense]): String = {
-    val outExp = ListMap(exp.toSeq.sortBy(_._1):_*)
-    val output = outExp.foldRight("")((x,acc) => acc ++ x._1.toString ++ ": " ++ x._2.toString++"\n")
+  def printExpenses(exp: Map[Int, Expense]): String = {
+    val outExp = ListMap(exp.toSeq.sortBy(_._1): _*)
+    val output = outExp.foldRight(header)((x, acc) => acc ++ x._1.toString ++ ": " ++ x._2.toString ++ "\n")
     output
   }
-//  def printExpenses(exp: Map[Int,Expense]): Map[Int,Expense] = {
-//    var outExp = ListMap(exp.toSeq.sortBy(_._1):_*)
-//    outExp.foreach(x=>println(x._1.toString++": "++x._2.toString))
-//    outExp
-//  }
 
-  def readExpense(): Map[Int,Expense] = {
-    var expensesMap = Map[Int,Expense]()
+  //  def printExpenses(exp: Map[Int,Expense]): Map[Int,Expense] = {
+  //    var outExp = ListMap(exp.toSeq.sortBy(_._1):_*)
+  //    outExp.foreach(x=>println(x._1.toString++": "++x._2.toString))
+  //    outExp
+  //  }
+
+  def readExpense(): Map[Int, Expense] = {
+    var expensesMap = Map[Int, Expense]()
 
     // read in file and make expenses
     val bufferedSource = Source.fromFile("data.csv") //TODO: data.csv -> datafile
     var i = 1
     for (line <- bufferedSource.getLines) {
-      if (!line.toUpperCase.contains("EXPENSE")){
+      if (!line.toUpperCase.contains("EXPENSE")) {
         expensesMap += (i -> makeExpense(line.toUpperCase))
-        i+=1
+        i += 1
+      } else {
+        header = line.toUpperCase++"\n"
       }
     }
     bufferedSource.close
@@ -144,8 +153,10 @@ object Expenses extends JFXApp {
       val tabPane = new TabPane()
 
       val cal = Calendar.getInstance()
-      val curMonth = "2020-01-01" //cal.get(Calendar.DATE).toString
-      val nextMonth = "2020-02-01" //cal.get(Calendar.DATE+30).toString
+      val year = cal.get(Calendar.YEAR)
+      val month = cal.get(Calendar.MONTH)+1
+      val curMonth = year.toString ++ "-" ++ month.toString ++ "-01"
+      val nextMonth = if(month == 12) (year+1).toString ++ "-01-01" else year.toString ++ "-" ++ (month+1).toString ++ "-01"
 
       // Reporting expenses
       val reportOutput = new TextArea {
@@ -153,12 +164,12 @@ object Expenses extends JFXApp {
         text = makeReport(expensesMap.values.toList, curMonth, nextMonth)
       }
 
-      val startDate = new DatePicker{
+      val startDate = new DatePicker {
         promptText = "Start date"
         maxWidth = buttonWidth
         editable = false
       }
-      val endDate = new DatePicker{
+      val endDate = new DatePicker {
         promptText = "End date"
         maxWidth = buttonWidth
         editable = false
@@ -167,43 +178,45 @@ object Expenses extends JFXApp {
       val report = new Button {
         text = "Report"
         onAction = _ => {
-          reportOutput.text = makeReport(expensesMap.values.toList,startDate.value.toString(),endDate.value.toString())
-          startDate.promptText = "Start date"
-          endDate.promptText = "End date"
+          reportOutput.text = makeReport(expensesMap.values.toList, startDate.value.value.toString, endDate.value.value.toString)
+          startDate.value = null
+          endDate.value = null
+//          startDate.promptText = "Start date"
+//          endDate.promptText = "End date"
         }
       }
 
-      val reportPane = new BorderPane{
+      val reportPane = new BorderPane {
         center = reportOutput
-        right = new VBox(startDate,endDate,report)
+        right = new VBox(startDate, endDate, report)
       }
 
-      val reportTab = new Tab{
+      val reportTab = new Tab {
         text = "Report"
         content = reportPane
         closable = false
       }
 
       // Adding expense
-      val addOutput = new TextArea{
+      val addOutput = new TextArea {
         editable = false
         text = printExpenses(expensesMap)
       }
 
-      val date = new DatePicker{
+      val date = new DatePicker {
         maxWidth = buttonWidth
         promptText = "Date"
         editable = false
       }
-      val expense = new TextField{
+      val expense = new TextField {
         maxWidth = buttonWidth
         promptText = "Amount"
       }
-      val section = new TextField{
+      val section = new TextField {
         maxWidth = buttonWidth
         promptText = "Type"
       }
-      val description = new TextField{
+      val description = new TextField {
         maxWidth = buttonWidth
         promptText = "Description"
       }
@@ -211,69 +224,82 @@ object Expenses extends JFXApp {
       val add: Node = new Button {
         text = "Add"
         onAction = _ => {
-          expensesMap = addExpense(expensesMap,date.value.toString,expense.text.toString.toDouble,section.text.toString(),description.text.toString)
+          expensesMap = addExpense(expensesMap, date.value.value.toString, expense.text.value.toDouble, section.text.value, description.text.value)
           addOutput.text = printExpenses(expensesMap)
-          addTab.content = addOutput
-          date.promptText = "Date"
-          expense.promptText = "Amount"
-          section.promptText = "Type"
-          description.promptText = "Description"
+//          addTab.content = addOutput
+          date.value = null
+          expense.text = null
+          section.text = null
+          description.text = null
+//          date.promptText = "Date"
+//          expense.promptText = "Amount"
+//          section.promptText = "Type"
+//          description.promptText = "Description"
         }
       }
 
-      val addPane = new BorderPane{
-        center = addOutput
-        right = new VBox(date,expense,section,description,add)
+      val addRefresh: Node = new Button {
+        text = "Refresh"
+        onAction = _ => {
+          addOutput.text = printExpenses(expensesMap)
+        }
       }
 
-      val addTab = new Tab{
+      val addPane = new BorderPane {
+        center = addOutput
+        right = new VBox(date, expense, section, description, add,addRefresh)
+      }
+
+      val addTab = new Tab {
         text = "Add"
         content = addPane
         closable = false
       }
 
       // Removing expense
-      val rmOutput = new TextArea{
+      val rmOutput = new TextArea {
         editable = false
         text = printExpenses(expensesMap)
       }
 
-      val rmIdx = new TextField{
+      val rmIdx = new TextField {
         maxWidth = buttonWidth
-        promptText = "ID of expense"
+        promptText = "ID(s)"
       }
 
       val rm: Node = new Button {
         text = "Remove"
         onAction = _ => {
-          expensesMap = rmExpense(expensesMap,rmIdx.toString.toInt)
+//          rmIdx.text.value.split(",").foreach(x=>expensesMap = rmExpense(expensesMap, x.toInt))
+          for(x <- rmIdx.text.value.split(",")){
+            expensesMap = rmExpense(expensesMap, x.toInt)
+          }
           rmOutput.text = printExpenses(expensesMap)
-          rmTab.content = rmOutput
-          rmIdx.promptText = "ID of expense"
+          rmIdx.text = null
+//          rmIdx.promptText = "ID of expense"
         }
       }
 
-      val rmPane = new BorderPane{
-        center = rmOutput
-        right = new VBox(rmIdx,rm)
+      val rmRefresh: Node = new Button {
+        text = "Refresh"
+        onAction = _ => {
+          rmOutput.text = printExpenses(expensesMap)
+        }
       }
 
-      val rmTab = new Tab{
+      val rmPane = new BorderPane {
+        center = rmOutput
+        right = new VBox(rmIdx, rm,rmRefresh)
+      }
+
+      val rmTab = new Tab {
         text = "Remove"
         content = rmPane
         closable = false
       }
 
-      tabPane.tabs = List(reportTab,addTab,rmTab)
+      tabPane.tabs = List(reportTab, addTab, rmTab)
       root = tabPane
-//      root = new BorderPane {
-//        right = new VBox(report,add,rm,print){
-//          padding = Insets(10)
-//        }
-//        top = new HBox(startDate,endDate) // TODO
-//        center = output // TODO
-//      }
     }
   }
-
 }
